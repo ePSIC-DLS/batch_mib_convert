84020485fa08d97c6a321018e5484e8f4794fd91
Mohsen Danaie <mohsen.danaie@diamond.ac.uk>
2022-10-07 12:39:02 +0100

    fixing version logging bug.

--- mib2hdf_watch_convert.py
+++ mib2hdf_watch_convert.py
@@ -30,8 +30,9 @@ logging.getLogger().setLevel("DEBUG")
 logger = logging.getLogger(__name__)
 
 # Log the version of code we are running.
-from batch_mib_convert import __version__
-logger.debug(f"{__name__} version {__version__}")
+import __version__
+print(__version__.__version__)
+logger.debug(f"{__name__} version {__version__.__version__}")
 
 # ---------------------------------------------------------------
 
